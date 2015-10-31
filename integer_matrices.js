e4723a820ee5d71bdfda99db36bd61063a81f7f2
Eric Campbell <1234goal@gmail.com>
2015-10-30 19:52:58 -0700

    rewrite using sylvester

--- integer_matrices.js
+++ integer_matrices.js
@@ -1,78 +1,32 @@
+var $ = require("sylvester")
 
-function determinant(matrixP){
-  fixRow = 0
-  det = 0
+var P = $M([
+  [1,7,3],
+  [9,4,0],
+  [2,7,1]
+])
 
-  /*if (matrixP.length = 2){
-    console.log("base case 2 : " + matrixP)
-    return twoSquareDet(matrixP)
-  } else*/
-  if (matrixP.length = 1) {
-    console.log("base case 1: " + matrixP)
-    return matrixP[fixRow][0]
+// Generate eigenvalues
+console.log(P)
+var k = P.determinant()
+var N = P.cols()
+console.log("You have chosen 1 - " + k + ", 1, and 1 + " + k + " as your eigenvalues")
+
+eigenvalues = [1-k, 1, 1+k]
+
+// create D
+var D = $.Matrix.Diagonal(eigenvalues)
+
+// create A
+var A = P.x(D).x(P.inv())
+
+function charPoly(matrix, eVals){
+  var poly = "";
+  for(var i = 0; i < eVals.length; i++){
+    var lam = eVals[i]
+    poly += "(&Lambda - " + lam + ")"
   }
-
-
-  console.log(matrixP)
-
-  for (var i = 0 ; i < matrixP.length ; i++){
-    cofactor = sign(i, fixRow) + determinant(minor(matrixP, i, fixRow))
-    pCurr = matrixP[fixRow][i]
-    console.log("cofactor : " + cofactor)
-    console.log("a_{ij} : " + pCurr)
-    det += pCurr * cofactor
-  }
-  return det
+  return poly
 }
 
-function sign(i,j){
-  return Math.pow(-1, i+j)
-}
-
-function minor(matrix, skipCol, skipRow){
-  cfm = []
-  for (var row = 0; row < matrix.length; row++){
-    if (row === skipRow){
-      continue
-    } else{
-      cfm.push([])
-      for (var col = 0; col < matrix[row].length; col++){
-        if (col === skipCol){
-          continue
-        }else if (row > skipRow){
-          cfm[row-1].push(matrix[row][col])
-        } else{
-          cfm[row].push(matrix[row][col])
-        }
-      }
-    }
-  }
-  return cfm
-}
-
-function twoSquareDet(matrix){
-  if (matrix.length = 2){
-    return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
-  } else {
-    return NaN
-  }
-}
-
-M = [
-      [1,2,3],
-      [2,3,5],
-      [3,6,1]
-    ]
-m = [
-      [1,3],
-      [5,4]
-    ]
-
-console.log(M)
-console.log(determinant(M))
-
-console.log(minor(m, 0, 0))
-console.log(minor(m, 0, 1))
-
-console.log(twoSquareDet(m))
-console.log(determinant(m))
+console.log(charPoly(A, eigenvalues))
