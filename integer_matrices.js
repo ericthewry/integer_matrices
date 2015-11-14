35359a6fcb5fa693bdd28cbe0eeb8fe01a5e62f6
Eric Campbell <1234goal@gmail.com>
2015-11-14 01:11:36 -0800

    fix bootstrap

--- integer_matrices.js
+++ integer_matrices.js
@@ -3,45 +3,6 @@ var $ = require("sylvester")
 PRECISION = 5
 RESIDUE = 7
 
-  // P = $M(arrayFromString(result.matrix))
-
-  // Generate eigenvalues
-  // var k = Math.abs(P.determinant())
-
-  // if (k === 0){
-    // fix the matrix so its not diagonalizable
-    // console.log("ERROR: Determinant of Matrix cannot be Zero")
-  // } else {
-    // var N = P.cols()
-    // var message = "\nYou have chosen "
-    // var eigenvalues = []
-
-    // for(var i = -1; i < N-1; i++){
-      // if(i < 0){
-        // message += "(" + RESIDUE + " - " + (k) +"), "
-      // } else if (i === 0) {
-        // message += "(" + RESIDUE + ")"
-//       } else {
-//         message += ", (" + RESIDUE + " + " + (i*k) + ")"
-//       }
-
-//       eigenvalues.push(RESIDUE+i*k)
-//     }
-
-//     console.log(message)
-
-//     // create D
-//     var D = $.Matrix.Diagonal(eigenvalues)
-
-//     console.log("\nThe diagonalized matrix: \n" + (D.inspect()))
-
-//     // create A = PD(P^-1)
-//     var A = P.x(D).x(P.inv().x(k).round()).x(1/k)
-//     console.log("\n The final matrix is \n" + A.inspect()  )
-//     console.log("\n Its characteristic polynomial is \n\t" + charPoly(A, eigenvalues))
-//   }
-// })
-
 exports.determinant = function(matrix){
   return matrix.determinant();
 }
