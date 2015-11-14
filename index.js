528da10ff199e8594733dc5c8607328fc978c442
Eric Campbell <1234goal@gmail.com>
2015-11-14 00:16:37 -0800

    set display matrix

--- index.js
+++ index.js
@@ -7,21 +7,36 @@ console.log('hello world');
 $(document).ready(
   function(){
     console.log("page loaded");
-    $("#input-matrix").bind('input',
-      function(){
-        console.log('input');
-        var string = $(this).text();
-        var P = mHelp.matrixFromString(string);
-        var detP = P.determinant();
-        $("#det").text("" + detP);
-        eigens = mHelp.eigenvalues(P.rows(), detP, 1)
-        console.log(eigens);
-        setEigenvalues(eigens);
-      }
-    )
+    runIMIES("#input-matrix");
+    $("#input-matrix").bind('input', function(){ runIMIES(this) });
   }
 )
 
+runIMIES = function(matrix){
+  var string = $(matrix).text();
+  var P = mHelp.matrixFromString(string);
+  var detP = P.determinant();
+  $("#det").text("" + detP);
+  var eigens = mHelp.eigenvalues(P.rows(), detP, 1)
+  setEigenvalues(eigens);
+  var diag = mHelp.diagonal(eigens);
+  setMatrix(mHelp.roundedProduct(P, diag, detP));
+}
+
+setMatrix = function(matrix){
+  html = '';
+  console.log(matrix);
+  // indexes into array begin at 1
+  for(var i = 1; i <= matrix.rows(); i++){
+    for(var j = 1; j <= matrix.cols(); j++){
+      html += matrix.e(i,j)
+      if (j - matrix.cols()) { html += " , " }
+    }
+    html += "<br/>"
+  }
+  console.log(html);
+  $("#final-matrix").html(html);
+}
 
 setEigenvalues = function(eigens){
   for(var i = 0; i < eigens.length; i++){
