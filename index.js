5f67ca388755bc8efeeeba61e9ab4237af9a084b
Eric Campbell <1234goal@gmail.com>
2015-11-13 21:50:27 -0800

    enable jquery read from editable div

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ window.jQuery = window.$ =  require('jquery');
 var bs = require('bootstrap-webpack');
 
 console.log('hello world');
+
+$(document).ready(
+  function(){
+    console.log("page loaded");
+    $("#input-matrix").bind('input',
+      function(){
+        console.log($(this).text())
+      }
+    )
+  }
+)
